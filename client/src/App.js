68179b2ed13279a70aa363cb988dd81487bbe350
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2023-05-02 21:59:44 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Form from "./modules/Form";
 
 function App() {
-  return <div className="">Hello World!</div>;
+  return (
+    <div className="bg-[#edf3fc] h-screen">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
