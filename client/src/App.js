6f8f64779fc0f26807f168362ff81dab0a2ae171
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2023-05-03 21:10:53 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Form from "./modules/Form";
 
 function App() {
   return (
-    <div className="bg-[#e1edff] h-screen">
+    <div className="bg-[#e1edff] h-screen flex justify-center items-center">
       <Form />
     </div>
   );
