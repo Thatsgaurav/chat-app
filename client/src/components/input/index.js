988310d0bae655fde0169240af4535a27673a54a
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2023-05-04 21:28:06 +0530

    commit

---
+++ client/src/components/input/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Input = () => {
+  label = "";
+  return;
+  <div>
+    <label
+      for="first_name"
+      class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+    >
+      {}
+    </label>
+  </div>;
+};
+
+export default Input;
