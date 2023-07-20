f0dd866f5aeaa5fc97c8efdb92a15c6a14fe2fba
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2023-07-20 22:44:00 +0530

    commit

---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
+
+const CustomerHeader = () => {
+  return (
+    <div className="chat-header">
+      <div></div>
+    </div>
+  );
+};
+
+export default CustomerHeader;
